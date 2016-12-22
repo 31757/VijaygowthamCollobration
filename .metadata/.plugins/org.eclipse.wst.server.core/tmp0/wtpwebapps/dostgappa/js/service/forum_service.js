ddbdafb905a87dcde0f7f3fab7ed3f6fbcd31a04
31757 <yashyajnesh@gmail.com>
2016-12-22 18:39:07 +0530

    Final Project 2

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/dostgappa/js/service/forum_service.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('myApp').factory('ForumService',
+		[ '$http', '$q', function($http, $q) {
+
+			var REST_SERVICE_URI = 'http://localhost:9050/vanuchat/fourm';
+			
+
+			var factory = {
+				fetchAllForum : fetchAllForum,
+				createForum : createForum,
+				updateForum : updateForum,
+				
+		
+				
+			};
+			return factory;
+			function fetchAllForum() {
+				var deferred = $q.defer();
+				$http.get(REST_SERVICE_URI).then(function(response) {
+					deferred.resolve(response.data);
+				}, function(errResponse) {
+					console.error('Error while fetching Forums');
+					deferred.reject(errResponse);
+				});
+				return deferred.promise;
+			}
+			function createForum(Forum) {
+				var deferred = $q.defer();
+				$http.post(REST_SERVICE_URI, Forum).then(function(response) {
+					deferred.resolve(response);
+				}, function(errResponse) {
+					console.error('Error while creating Forum');
+					deferred.reject(errResponse);
+				});
+				return deferred.promise;
+			}
+			function updateForum(Forum, id) {
+				var deferred = $q.defer();
+				$http.put(REST_SERVICE_URI + id, Forum).then(function(response) {
+					deferred.resolve(response.data);
+				}, function(errResponse) {
+					console.error('Error while updating Forum');
+					deferred.reject(errResponse);
+				});
+				return deferred.promise;
+			}
+			function postforumComments(Forum, id) {
+				var deferred = $q.defer();
+				$http.put(REST_SERVICE_URI + id, Forum).then(function(response) {
+					deferred.resolve(response.data);
+				}, function(errResponse) {
+					console.error('Error while updating Forum');
+					deferred.reject(errResponse);
+				});
+				return deferred.promise;
+			}
+			
+			
+		} ]);
--- dostgappachat/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Dost Gappa</h2>
-</body>
-</html>
